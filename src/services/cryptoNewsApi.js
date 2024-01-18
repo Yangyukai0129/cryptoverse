615331128506909fdfe8d531a0d0ca1a0a8058b1
kevin_yang <kevin_yang@comwave.com.tw>
2024-01-18 14:38:49 +0800

    更新api地址

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery, } from '@reduxjs/toolkit/query/react';
 
-const baseUrl = 'd'
-// 'https://news67.p.rapidapi.com';
+const baseUrl = 'https://news67.p.rapidapi.com';
 
 export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
